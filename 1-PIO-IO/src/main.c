9d57a7e858bf2ceb156fbd48469b235f185a1e54
listerogusuku <listeror@al.insper.edu.br>
2023-02-19 22:35:34 -0300

    Sobe Lab1-PIO finalziado

--- 1-PIO-IO/src/main.c
+++ 1-PIO-IO/src/main.c
@@ -23,10 +23,64 @@
 /* defines                                                              */
 /************************************************************************/
 
-#define LED_PIO           
-#define LED_PIO_ID        
-#define LED_PIO_IDX       
-#define LED_PIO_IDX_MASK  
+// LED E BOTÃO DA PLACA
+
+
+#define LED_PIO           PIOC                 // periferico que controla o LED
+// # (1)
+#define LED_PIO_ID        ID_PIOC                  // ID do periférico PIOC (controla LED)
+#define LED_PIO_IDX       8                    // ID do LED no PIO
+#define LED_PIO_IDX_MASK  (1 << LED_PIO_IDX)   // Mascara para CONTROLARMOS o LED
+
+// Configuracoes do botao
+#define BUT_PIO				PIOA
+#define BUT_PIO_ID			ID_PIOA
+#define BUT_PIO_IDX			11
+#define BUT_PIO_IDX_MASK (1u << BUT_PIO_IDX) // esse já está pronto.
+
+
+// LEDS E BOTÕES DO OLED
+
+//LED E BOTÃO 1:
+
+#define LED1_PIO           PIOA                 // periferico que controla o LED
+#define LED1_PIO_ID        ID_PIOA                  // ID do periférico PIOC (controla LED)
+#define LED1_PIO_IDX       0                    // ID do LED no PIO (PIO A PINO 0)
+#define LED1_PIO_IDX_MASK  (1 << LED1_PIO_IDX)   // Mascara para CONTROLARMOS o LED
+
+// Configuracoes do botao
+#define BUT1_PIO			PIOD
+#define BUT1_PIO_ID			ID_PIOD
+#define BUT1_PIO_IDX		28
+#define BUT1_PIO_IDX_MASK (1u << BUT1_PIO_IDX)
+
+
+//LED E BOTÃO 2:
+
+#define LED2_PIO           PIOC                 // periferico que controla o LED
+#define LED2_PIO_ID        ID_PIOC                  // ID do periférico PIOC (controla LED)
+#define LED2_PIO_IDX       30                    // ID do LED no PIO (PIO A PINO 0)
+#define LED2_PIO_IDX_MASK  (1 << LED2_PIO_IDX)   // Mascara para CONTROLARMOS o LED
+
+// Configuracoes do botao
+#define BUT2_PIO			PIOC
+#define BUT2_PIO_ID			ID_PIOC
+#define BUT2_PIO_IDX		31
+#define BUT2_PIO_IDX_MASK (1u << BUT2_PIO_IDX)
+
+
+//LED E BOTÃO 3:
+
+#define LED3_PIO           PIOB                 // periferico que controla o LED
+#define LED3_PIO_ID        ID_PIOB                  // ID do periférico PIOC (controla LED)
+#define LED3_PIO_IDX       2                    // ID do LED no PIO (PIO A PINO 0)
+#define LED3_PIO_IDX_MASK  (1 << LED3_PIO_IDX)   // Mascara para CONTROLARMOS o LED
+
+// Configuracoes do botao
+#define BUT3_PIO			PIOA
+#define BUT3_PIO_ID			ID_PIOA
+#define BUT3_PIO_IDX		19
+#define BUT3_PIO_IDX_MASK (1u << BUT3_PIO_IDX)
 
 /************************************************************************/
 /* constants                                                            */
@@ -51,8 +105,42 @@ void init(void);
 /************************************************************************/
 
 // Função de inicialização do uC
-void init(void)
-{
+void init(void){
+	// Inicializa clock
+	sysclk_init();
+
+	// Desativa watchdog
+	WDT->WDT_MR = WDT_MR_WDDIS;
+
+	// Ativa PIOs
+	// LED E BOTÃO DA PLACA
+	pmc_enable_periph_clk(LED_PIO_ID);
+	pmc_enable_periph_clk(BUT_PIO_ID);
+
+	// LED 1 E BOTÃO 1 DO OLED
+	pmc_enable_periph_clk(LED1_PIO_ID);
+	pmc_enable_periph_clk(BUT1_PIO_ID);
+
+	// LED 2 E BOTÃO 2 DO OLED
+	pmc_enable_periph_clk(LED2_PIO_ID);
+	pmc_enable_periph_clk(BUT2_PIO_ID);
+
+	// LED 3 E BOTÃO 3 DO OLED
+	pmc_enable_periph_clk(LED3_PIO_ID);
+	pmc_enable_periph_clk(BUT3_PIO_ID);
+
+	// Configura Pinos
+	pio_configure(LED_PIO, PIO_OUTPUT_0, LED_PIO_IDX_MASK, PIO_DEFAULT);
+	pio_configure(BUT_PIO, PIO_INPUT, BUT_PIO_IDX_MASK, PIO_PULLUP);
+
+	pio_configure(LED1_PIO, PIO_OUTPUT_0, LED1_PIO_IDX_MASK, PIO_DEFAULT);
+	pio_configure(BUT1_PIO, PIO_INPUT, BUT1_PIO_IDX_MASK, PIO_PULLUP);
+
+	pio_configure(LED2_PIO, PIO_OUTPUT_0, LED2_PIO_IDX_MASK, PIO_DEFAULT);
+	pio_configure(BUT2_PIO, PIO_INPUT, BUT2_PIO_IDX_MASK, PIO_PULLUP);
+
+	pio_configure(LED3_PIO, PIO_OUTPUT_0, LED3_PIO_IDX_MASK, PIO_DEFAULT);
+	pio_configure(BUT3_PIO, PIO_INPUT, BUT3_PIO_IDX_MASK, PIO_PULLUP);
 
 }
 
@@ -67,9 +155,57 @@ int main(void)
 
   // super loop
   // aplicacoes embarcadas não devem sair do while(1).
-  while (1)
-  {
+  while(1) {
+	  // Verifica valor do pino que o botão está conectado
+	  if(!pio_get(BUT_PIO, PIO_INPUT, BUT_PIO_IDX_MASK)) {
+		  // Pisca LED
+		  for (int i=0; i<10; i++) {
+			  pio_clear(LED_PIO, LED_PIO_IDX_MASK);  // Limpa o pino LED_PIO_PIN
+			  delay_ms(100);                         // delay
+			  pio_set(LED_PIO, LED_PIO_IDX_MASK);    // Ativa o pino LED_PIO_PIN
+			  delay_ms(100);                         // delay
+		  }
+		  } else  {
+		  // Ativa o pino LED_IDX (par apagar)
+		  pio_set(LED_PIO, LED_PIO_IDX_MASK);
+	  }
+	if(!pio_get(BUT1_PIO, PIO_INPUT, BUT1_PIO_IDX_MASK)) {
+		// Pisca LED
+		for (int i=0; i<10; i++) {
+			pio_clear(LED1_PIO, LED1_PIO_IDX_MASK);  // Limpa o pino LED_PIO_PIN
+			delay_ms(100);                         // delay
+			pio_set(LED1_PIO, LED1_PIO_IDX_MASK);    // Ativa o pino LED_PIO_PIN
+			delay_ms(100);                         // delay
+		}
+		} else  {
+		// Ativa o pino LED_IDX (par apagar)
+		pio_set(LED1_PIO, LED1_PIO_IDX_MASK);
+	}
 
+	if(!pio_get(BUT2_PIO, PIO_INPUT, BUT2_PIO_IDX_MASK)) {
+		// Pisca LED
+		for (int i=0; i<10; i++) {
+			pio_clear(LED2_PIO, LED2_PIO_IDX_MASK);  // Limpa o pino LED_PIO_PIN
+			delay_ms(100);                         // delay
+			pio_set(LED2_PIO, LED2_PIO_IDX_MASK);    // Ativa o pino LED_PIO_PIN
+			delay_ms(100);                         // delay
+		}
+		} else  {
+		// Ativa o pino LED_IDX (par apagar)
+		pio_set(LED2_PIO, LED2_PIO_IDX_MASK);
+	}
+	if(!pio_get(BUT3_PIO, PIO_INPUT, BUT3_PIO_IDX_MASK)) {
+		// Pisca LED
+		for (int i=0; i<10; i++) {
+			pio_clear(LED3_PIO, LED3_PIO_IDX_MASK);  // Limpa o pino LED_PIO_PIN
+			delay_ms(100);                         // delay
+			pio_set(LED3_PIO, LED3_PIO_IDX_MASK);    // Ativa o pino LED_PIO_PIN
+			delay_ms(100);                         // delay
+		}
+		} else  {
+		// Ativa o pino LED_IDX (par apagar)
+		pio_set(LED3_PIO, LED3_PIO_IDX_MASK);
+	}
   }
   return 0;
 }
